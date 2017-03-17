7053a37b12991b54aae2d52eb759c45140016dc6
Eden <edenstevens101@gmail.com>
2017-03-17 14:59:43 +0000

    added three rooms

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -6,13 +6,25 @@ require_relative('../guest.rb')
 class RoomTest < MiniTest::Test
 
   def setup()
-    @room = Room.new
     @guest1 = Guest.new("Eden", 30)
     @guest2 = Guest.new("John", 60)
+    @room = Room.new(["Steven", "Jonah"], ["Lucy", "Arthur"], ["Laura", "Kaitlin"])
   end
 
-  def test_empty_room()
-    assert_equal(0, @room.passenger_count())
+  def test_total_guest_count()
+    assert_equal(6, @room.total_guest_count())
+  end
+
+  def test_total_guests_in_chill_count()
+    assert_equal(2, @room.chill_room_count())
+  end
+
+  def test_total_guest_in_rock_count()
+    assert_equal(2, @room.rock_room_count())
+  end
+
+  def test_total_guest_in_pop_count()
+    assert_equal(2, @room.pop_room_count())
   end
 
 end
