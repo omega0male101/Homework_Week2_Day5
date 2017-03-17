74c568f50340b0c8d77d840bc1a9a6dace5ab0af
Eden <edenstevens101@gmail.com>
2017-03-17 16:24:27 +0000

    Added more room functions 'adding guests'

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -8,11 +8,11 @@ class RoomTest < MiniTest::Test
   def setup()
     @guest1 = Guest.new("Eden", 30)
     @guest2 = Guest.new("John", 60)
-    @room = Room.new(["Steven", "Jonah"], ["Lucy", "Arthur"], ["Laura", "Kaitlin"])
+    @room = Room.new(["Steven", "Jonah"], ["Lucy", "Oscar", "Arthur"], ["Laura", "Kaitlin"])
   end
 
   def test_total_guest_count()
-    assert_equal(6, @room.total_guest_count())
+    assert_equal(7, @room.total_guest_count())
   end
 
   def test_total_guests_in_chill_count()
@@ -20,11 +20,27 @@ class RoomTest < MiniTest::Test
   end
 
   def test_total_guest_in_rock_count()
-    assert_equal(2, @room.rock_room_count())
+    assert_equal(3, @room.rock_room_count())
   end
 
   def test_total_guest_in_pop_count()
     assert_equal(2, @room.pop_room_count())
   end
 
+  def test_add_to_room()
+    @room.empty()
+
+    @room.add_to_room(@guest1, "rock")
+     assert_equal(1, @room.rock_room_count)
+    @room.add_to_room(@guest1, "pop")
+     assert_equal(1, @room.pop_room_count)
+    @room.add_to_room(@guest1, "chill")
+     assert_equal(1, @room.chill_room_count)
+  end
+
+  def test_empty()
+    @room.empty()
+    assert_equal(0, @room.total_guest_count)
+  end
+  
 end
